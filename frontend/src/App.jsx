4d19a88b5897fa3cb805793a23413454cf1bbe82
Mayank Mehta <mayankmehta19032003@gmail.com>
2024-09-17 02:43:14 +0530

    cleaning all deafault code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,11 +5,9 @@ function App() {
 
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
 
-export default App
+export default App;

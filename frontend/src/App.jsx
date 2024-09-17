0ea7d73d7721e00e7f8c1d73a493186f22cf0cc6
Mayank Mehta <mayankmehta19032003@gmail.com>
2024-09-17 16:03:19 +0530

    home page UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,90 +1,34 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeItem from "./components/HomeItem";
+
 function App() {
 
+    const item = {
+        id: '001',
+        image: 'images/1.jpg',
+        company: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    }
 
   return (
     <>
-    <header>
-        <div className="logo_container">
-            <a href="#"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home"/></a>
-        </div>
-        <nav className="nav_bar">
-            <a href="#">Men</a>
-            <a href="#">Women</a>
-            <a href="#">Kids</a>
-            <a href="#">Home & Living</a>
-            <a href="#">Beauty</a>
-            <a href="#">Studio <sup>New</sup></a>
-        </nav>
-        <div className="search_bar">
-            <span className="material-symbols-outlined search_icon">search</span>
-            <input className="search_input" placeholder="Search for products, brands and more"/>
-        </div>
-        <div className="action_bar">
-            <div className="action_container">
-                <span className="material-symbols-outlined action_icon">person</span>
-                <span className="action_name">Profile</span>
-            </div>
-
-            <div className="action_container">
-                <span className="material-symbols-outlined action_icon">favorite</span>
-                <span className="action_name">Wishlist</span>
-            </div>
-
-            <a className="action_container" href="pages/bag.html">
-                <span className="material-symbols-outlined action_icon">shopping_bag</span>
-                <span className="action_name">Bag</span>
-                <span className="bag-item-count">0</span>
-            </a>
-        </div>
-    </header>
+    <Header/>
     <main>
         <div className="items-container">
+            <HomeItem item={item}/>
         </div>
     </main>
-    <footer>
-        <div className="footer_container">
-            <div className="footer_column">
-                <h3>ONLINE SHOPPING</h3>
-
-                <a href="#">Men</a>
-                <a href="#">Women</a>
-                <a href="#">Kids</a>
-                <a href="#">Home & Living</a>
-                <a href="#">Beauty</a>
-                <a href="#">Gift Card</a>
-                <a href="#">Myntra Insider</a>
-            </div>
-
-            <div className="footer_column">
-                <h3>ONLINE SHOPPING</h3>
-
-                <a href="#">Men</a>
-                <a href="#">Women</a>
-                <a href="#">Kids</a>
-                <a href="#">Home & Living</a>
-                <a href="#">Beauty</a>
-                <a href="#">Gift Card</a>
-                <a href="#">Myntra Insider</a>
-            </div>
-
-            <div className="footer_column">
-                <h3>ONLINE SHOPPING</h3>
-
-                <a href="#">Men</a>
-                <a href="#">Women</a>
-                <a href="#">Kids</a>
-                <a href="#">Home & Living</a>
-                <a href="#">Beauty</a>
-                <a href="#">Gift Card</a>
-                <a href="#">Myntra Insider</a>
-            </div>
-        </div>
-        <hr/>
-
-        <div className="copyright">
-            © 2023 www.myntra.com. All rights reserved.
-        </div>
-    </footer>
+   <Footer/>
     </>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { IoHeartOutline } from "react-icons/io5";
+import { BsHandbag } from "react-icons/bs";
+
+function Header(){
+    return(
+<header>
+        <div className="logo_container">
+            <a href="#"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home"/></a>
+        </div>
+        <nav className="nav_bar">
+            <a href="#">Men</a>
+            <a href="#">Women</a>
+            <a href="#">Kids</a>
+            <a href="#">Home & Living</a>
+            <a href="#">Beauty</a>
+            <a href="#">Studio <sup>New</sup></a>
+        </nav>
+        <div className="search_bar">
+            <span className="material-symbols-outlined search_icon">search</span>
+            <input className="search_input" placeholder="Search for products, brands and more"/>
+        </div>
+        <div className="action_bar">
+            <div className="action_container">
+            <IoPersonCircleOutline />
+                <span className="action_name">Profile</span>
+            </div>
+
+            <div className="action_container">
+          < IoHeartOutline/>
+                <span className="action_name">Wishlist</span>
+            </div>
+
+            <a className="action_container" href="pages/bag.html">
+             <BsHandbag/>
+                <span className="action_name">Bag</span>
+                <span className="bag-item-count">0</span>
+            </a>
+        </div>
+    </header>
+    )
+}
+export default Header;
